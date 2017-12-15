909f57b95d2fa5689e4a598cf2cd866f845494b2
Jonathan Laflamme <jonathan.laflamme.jl@gmail.com>
2017-12-15 01:40:04 -0500

    Installed ReactJS

---
+++ src/components/chat.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class Chat extends Component {
+    render() {
+        return (
+            <p>Yoo dude</p>
+        )
+    }
+}
+
+export default Chat;
